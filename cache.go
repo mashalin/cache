e323f4cd27a973dcb6558e1683c176108564c0c8
Masha Trushel <trushel.maria@crossnetics.io>
2024-08-22 21:19:34 +0300

    added mutex and ttl

--- cache.go
+++ cache.go
@@ -1,23 +1,58 @@
 package cache
 
+import (
+	"errors"
+	"sync"
+	"time"
+)
+
 type Cache struct {
-	data map[string]interface{}
+	data       map[string]interface{}
+	expiration map[string]time.Time
+	mu         sync.Mutex
 }
 
 func New() *Cache {
 	return &Cache{
-		data: make(map[string]interface{}),
+		data:       make(map[string]interface{}),
+		expiration: make(map[string]time.Time),
 	}
 }
 
-func (c *Cache) Set(key string, value interface{}) {
+func (c *Cache) Set(key string, value interface{}, ttl time.Duration) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
 	c.data[key] = value
+	if ttl > 0 {
+		c.expiration[key] = time.Now().Add(ttl)
+	} else {
+		delete(c.expiration, key)
+	}
 }
 
-func (c Cache) Get(key string) interface{} {
-	return c.data[key]
+func (c *Cache) Get(key string) (interface{}, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if expiration, ok := c.expiration[key]; ok {
+		if time.Now().After(expiration) {
+			c.deleteWithoutLock(key)
+			return nil, errors.New("no such value")
+		}
+	}
+
+	return c.data[key], nil
 }
 
 func (c *Cache) Delete(key string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.deleteWithoutLock(key)
+}
+
+func (c *Cache) deleteWithoutLock(key string) {
 	delete(c.data, key)
+	delete(c.expiration, key)
 }
